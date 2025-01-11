09b79916e9456b1310055cb585fe798db781f8c4
Nurul Ashraf <88407376+nurulashraf@users.noreply.github.com>
2025-01-12 02:13:21 +0800

    Create Exercise4.java

---
+++ loops/Exercise4.java
@@ -0,0 +1,11 @@
+// This code prints 5 numbers horizontally using 'for' loop.
+
+package exercise4;
+
+public class Exercise4 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.print(i);
+        }
+    }
+}
