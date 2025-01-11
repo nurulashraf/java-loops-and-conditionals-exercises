bb629cc895da503c5989600dc49e60c54862fb7c
Nurul Ashraf <88407376+nurulashraf@users.noreply.github.com>
2025-01-12 02:09:39 +0800

    Create Exercise3.java

---
+++ loops/Exercise3.java
@@ -0,0 +1,15 @@
+// This code prints 5 numbers vertically using 'do... while' loop.
+
+package exercise3;
+
+public class Exercise3 {
+
+    public static void main(String[] args) {    
+        int i = 1; 
+
+        do {    
+            System.out.println(i);    
+            i++;   
+        } while (i <= 5);    
+    }
+}
