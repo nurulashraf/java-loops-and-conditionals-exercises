587845c8072181e3dd714a2e6e3a9a79d3262927
Nurul Ashraf <88407376+nurulashraf@users.noreply.github.com>
2025-01-12 02:05:01 +0800

    Create Exercise1.java

---
+++ loops/Exercise1.java
@@ -0,0 +1,12 @@
+// This code prints 5 numbers vertically.
+
+package exercise1;
+
+public class Exercise1 {
+
+    public static void main(String[] args) {  
+        for(int i=1;i<=5;i++){
+            System.out.println(i);  
+        }  
+    }  
+}
