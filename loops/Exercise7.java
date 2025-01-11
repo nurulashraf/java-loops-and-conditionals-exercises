0505d905c256d8f96dcf46e17f2dbbffa31374b8
Nurul Ashraf <88407376+nurulashraf@users.noreply.github.com>
2025-01-12 02:43:25 +0800

    Create Exercise7.java

---
+++ loops/Exercise7.java
@@ -0,0 +1,28 @@
+/*
+  This code uses nested do-while loops to print the outer loop index (1 to 2), 
+  followed by the inner loop index (1 to 3) for each iteration of the outer loop.
+*/
+
+
+package exercise7;
+
+public class Exercise7 {
+
+    public static void main(String[] args) {
+        
+        int i = 1;
+        
+        do {
+            System.out.println("Outer: " + i);
+
+            int j = 1;
+            
+            do {
+                System.out.println(" Inner: " + j);
+                j++;
+            } while (j <= 3);
+            
+            i++;
+        } while (i <= 2);
+    }
+}
