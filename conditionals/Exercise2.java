46a51682b0da892b30e6e2c5da685aa620644137
Nurul Ashraf <88407376+nurulashraf@users.noreply.github.com>
2025-01-12 02:28:40 +0800

    Create Exercise2.java

---
+++ conditionals/Exercise2.java
@@ -0,0 +1,14 @@
+// This code compares two integers and prints the greatest one.
+
+package greatestnumber;
+
+public class GreatestNumber {
+    public static void main(String[] args) {
+        int num1 = 1, num2 = 2;
+        
+        if (num1 > num2)
+            System.out.println("The greatest: " + num1);
+        else
+            System.out.println("The greatest: " + num2);
+    }
+}
